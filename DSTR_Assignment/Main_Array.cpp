b0ef9b90ee8718704bf423a03cc3d5a9e426fe52
justintai <62212747+justintai@users.noreply.github.com>
2022-05-28 19:09:51 +0800

    completed hr menu

--- DSTR_Assignment/Main_Array.cpp
+++ DSTR_Assignment/Main_Array.cpp
@@ -32,6 +32,33 @@ time_t toTimeStamp(string date) {
 	return t_of_day;
 }
 
+// Checking Validation
+bool isDateNumber(const string& str) {
+	for (int i = 0; i < str.length(); i++) {
+		if (i != 4 && i != 7) {
+			if (isdigit(str[i]) == 0) return false;
+		}
+	}
+	return true;
+}
+
+bool checkDateValid(string date) {
+	if (date.length() != 10 || !isDateNumber(date) || stoi(date.substr(0, 4)) - 1900 < 0 ||
+		!(stoi(date.substr(5, 7)) - 1 >= 0 && stoi(date.substr(5, 7)) - 1 < 12) ||
+		!(stoi(date.substr(8, 10)) > 0 && stoi(date.substr(8, 10)) <= 31)) {
+		cout << " **Invalid format try again!" << endl;
+		return true;
+	}
+	return false;
+}
+
+bool isNumber(const string& str) {
+	for (int i = 0; i < str.length(); i++) {
+		if (isdigit(str[i]) == 0) return false;
+	}
+	return true;
+}
+
 // Struct of object
 struct Users {
 	int UserID;
@@ -106,6 +133,41 @@ public:
 
 		return name;
 	}
+
+	bool checkIDValid(int param) {
+		for (int i = 0; i < length; i++) {
+			if (centres[i].CentreCode == param) {
+				cout << " **The code is duplicated!" << endl;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void addCentreData(Centre nd) {
+		length++;
+		Centre* newData = new Centre[length];
+
+		for (int i = 0; i < length; i++) {
+			if (i != length - 1) {
+				newData[i].CentreCode = centres[i].CentreCode;
+				newData[i].Centre_Name = centres[i].Centre_Name;
+				newData[i].Address = centres[i].Address;
+				newData[i].Region = centres[i].Region;
+			}
+			else {
+				newData[i].CentreCode = nd.CentreCode;
+				newData[i].Centre_Name = nd.Centre_Name;
+				newData[i].Address = nd.Address;
+				newData[i].Region = nd.Region;
+			}
+		}
+
+		centres = newData;
+		cout << "Add new data success!" << endl;
+		return;
+	}
+
 }centredata;
 
 class SubjectData {
@@ -143,6 +205,37 @@ public:
 
 		return name;
 	}
+
+	bool checkIDValid(int param) {
+		for (int i = 0; i < length; i++) {
+			if (subjects[i].SubjectCode == param) {
+				cout << " **The code is duplicated!" << endl;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void addSubjectData(Subject nd) {
+		length++;
+		Subject* newData = new Subject[length];
+
+		for (int i = 0; i < length; i++) {
+			if (i != length - 1) {
+				newData[i].SubjectCode = subjects[i].SubjectCode;
+				newData[i].Subject_Name = subjects[i].Subject_Name;
+			}
+			else {
+				newData[i].SubjectCode = nd.SubjectCode;
+				newData[i].Subject_Name = nd.Subject_Name;
+			}
+		}
+
+		subjects = newData;
+		cout << "Add new data success!" << endl;
+		return;
+	}
+
 }subjectdata;
 
 class UserData {
@@ -155,7 +248,7 @@ public:
 		users = new Users[size];
 		Users data[3] = { {1, "sawchanghau", "sch123", "STUDENT", 1},
 						{2, "taihongyi", "thy123", "ADMIN", 1},
-						{3, "lau", "lm123", "HR MANAGER", 0} };
+						{3, "laurentius", "lm123", "HR MANAGER", 0} };
 
 		for (int i = 0; i < size; i++) {
 			users[i].UserID = data[i].UserID;
@@ -178,14 +271,24 @@ public:
 
 	int RoleConvert(string role) {
 		int no = 0;
-		if (role == "STUDENT" || role == "student")
+		if (role == "STUDENT" || role == "student" || role == "Student")
 			no = 1;
-		else if (role == "ADMIN" || role == "admin")
+		else if (role == "ADMIN" || role == "admin" || role == "Admin")
 			no = 2;
-		else if (role == "HR MANAGER" || role == "hr manager" || role == "hr" || role == "HR")
+		else if (role == "HR MANAGER" || role == "hr manager" || role == "hr" || role == "HR" || role == "Hr Manager")
 			no = 3;
 
 		return no;
+	}
+
+	bool checkIDValid(int param) {
+		for (int i = 0; i < length; i++) {
+			if (users[i].UserID == param) {
+				cout << " **The ID is duplicated!" << endl;
+				return true;
+			}
+		}
+		return false;
 	}
 
 	void addUserData(Users nd) {
@@ -193,7 +296,7 @@ public:
 		Users* newUsers = new Users[length];
 
 		for (int i = 0; i < length; i++) {
-			if (i != length) {
+			if (i != length - 1) {
 				newUsers[i].UserID = users[i].UserID;
 				newUsers[i].Username = users[i].Username;
 				newUsers[i].Password = users[i].Password;
@@ -210,6 +313,7 @@ public:
 		}
 
 		users = newUsers;
+		cout << "Add new data success!" << endl;
 	}
 }usersdata;
 
@@ -304,6 +408,16 @@ public:
 		cout << "Add new data success!" << endl;
 	}
 
+	bool checkIDValid(int param) {
+		for (int i = 0; i < length; i++) {
+			if (tutors[i].TutorID == param) {
+				cout << " **The ID is duplicated!" << endl;
+				return true;
+			}
+		}
+		return false;
+	}
+
 	// normal display
 	void display(int mode) {
 		// mode 0: normal display with arranged by tutor name
@@ -354,7 +468,7 @@ public:
 				string subName = subjectdata.convertSubjectCode(dump[i].Subject_Code);
 				cout << "| " << dump[i].TutorID << setw(s1 - to_string(dump[i].TutorID).length()) << setfill(separator)
 					<< "| " << dump[i].Name << setw(s2 - dump[i].Name.length()) << setfill(separator)
-					<< "| " << fixed << setprecision(2) << dump[i].Hourly_Rate
+					<< "| " << to_string(dump[i].Hourly_Rate).substr(0, 6)
 					<< setw(s3 - to_string(dump[i].Hourly_Rate).substr(0, 6).length()) << setfill(separator)
 					<< "| " << dump[i].Phone << setw(s4 - to_string(dump[i].Phone).length()) << setfill(separator)
 					<< "| " << centreName << setw(s5 - centreName.length()) << setfill(separator)
@@ -416,7 +530,7 @@ public:
 				string subName = subjectdata.convertSubjectCode(tutors[i].Subject_Code);
 				cout << "| " << temp[i].TutorID << setw(s1 - to_string(temp[i].TutorID).length()) << setfill(separator)
 					<< "| " << temp[i].Name << setw(s2 - temp[i].Name.length()) << setfill(separator)
-					<< "| " << fixed << setprecision(2) << temp[i].Hourly_Rate
+					<< "| " << to_string(temp[i].Hourly_Rate).substr(0, 6)
 					<< setw(s3 - to_string(temp[i].Hourly_Rate).substr(0, 6).length()) << setfill(separator)
 					<< "| " << temp[i].Phone << setw(s4 - to_string(temp[i].Phone).length()) << setfill(separator)
 					<< "| " << centreName << setw(s5 - centreName.length())
@@ -589,11 +703,12 @@ public:
 	}
 
 	void BinarySearch(int mode) {
-		auto starttime = high_resolution_clock::now();
 		// mode: 1 - search tutor id, 2 - search performance
 		Tutors* result = NULL;
 
 		if (mode == 1) {
+			BubbleSort(1); // binary search must use in assending order
+			auto starttime = high_resolution_clock::now();
 			int search, l = 0, r = length - 1;
 			int j = 0, m = 0;
 			cout << "Enter tutor ID: ";
@@ -601,7 +716,7 @@ public:
 
 			while (l <= r) {
 				Tutors* newResult;
-				int i = l + (r - 1) / 2;
+				int i = l + (r - l) / 2;
 
 				if (tutors[i].TutorID == search) {
 					j++;
@@ -661,7 +776,7 @@ public:
 			system("CLS");
 			auto stoptime = high_resolution_clock::now();
 			auto duration = duration_cast<microseconds>(stoptime - starttime);
-			cout << "Time Used:" << duration.count() << " microseconds" << endl;
+			cout << "Binary Search Time Used:" << duration.count() << " microseconds" << endl;
 
 			if (result != NULL) {
 				display(result, j); 
@@ -672,6 +787,8 @@ public:
 			return;
 		}
 		else if (mode == 2) {
+			BubbleSort(3); // binary search must is assending order
+			auto starttime = high_resolution_clock::now();
 			float search, l = 0, r = length - 1;
 			int j = 0, m = 0;
 			cout << "Enter overall performance rate: ";
@@ -679,7 +796,7 @@ public:
 
 			while (l <= r) {
 				Tutors* newResult;
-				int i = l + (r - 1) / 2;
+				int i = l + (r - l) / 2;
 
 				if (tutors[i].Rating == search) {
 					j++;
@@ -739,7 +856,7 @@ public:
 			system("CLS");
 			auto stoptime = high_resolution_clock::now();
 			auto duration = duration_cast<microseconds>(stoptime - starttime);
-			cout << "Time Used:" << duration.count() << " microseconds" << endl;
+			cout << "Binary Search Time Used:" << duration.count() << " microseconds" << endl;
 
 			if (result != NULL) {
 				display(result, j);
@@ -852,7 +969,7 @@ public:
 			system("CLS");
 			auto stoptime = high_resolution_clock::now();
 			auto duration = duration_cast<microseconds>(stoptime - starttime);
-			cout << "Time Used:" << duration.count() << " microseconds" << endl;
+			cout << "Bubble Sort Time Used:" << duration.count() << " microseconds" << endl;
 
 			display(1);
 		}
@@ -905,7 +1022,7 @@ public:
 			system("CLS");
 			auto stoptime = high_resolution_clock::now();
 			auto duration = duration_cast<microseconds>(stoptime - starttime);
-			cout << "Time Used:" << duration.count() << " microseconds" << endl;
+			cout << "Bubble Sort Time Used:" << duration.count() << " microseconds" << endl;
 
 			display(1);
 		}
@@ -958,7 +1075,7 @@ public:
 			system("CLS");
 			auto stoptime = high_resolution_clock::now();
 			auto duration = duration_cast<microseconds>(stoptime - starttime);
-			cout << "Time Used:" << duration.count() << " microseconds" << endl;
+			cout << "Bubble Sort Time Used:" << duration.count() << " microseconds" << endl;
 
 			display(1);
 		}
@@ -1019,7 +1136,7 @@ public:
 			system("CLS");
 			auto stoptime = high_resolution_clock::now();
 			auto duration = duration_cast<microseconds>(stoptime - starttime);
-			cout << "Time Used:" << duration.count() << " microseconds" << endl;
+			cout << "Insertion Sort Time Used:" << duration.count() << " microseconds" << endl;
 
 			display(1);
 		}
@@ -1073,7 +1190,7 @@ public:
 			system("CLS");
 			auto stoptime = high_resolution_clock::now();
 			auto duration = duration_cast<microseconds>(stoptime - starttime);
-			cout << "Time Used:" << duration.count() << " microseconds" << endl;
+			cout << "Insertion Sort Time Used:" << duration.count() << " microseconds" << endl;
 
 			display(1);
 		}
@@ -1127,24 +1244,205 @@ public:
 			system("CLS");
 			auto stoptime = high_resolution_clock::now();
 			auto duration = duration_cast<microseconds>(stoptime - starttime);
-			cout << "Time Used:" << duration.count() << " microseconds" << endl;
+			cout << "Insertion Sort Time Used:" << duration.count() << " microseconds" << endl;
 
 			display(1);
 		}
 	}
 
+	int BinarySearchForIndex(int search) {
+		InsertionSort(1); // Binary search must in tutor id assending
+		int l = 0, r = length - 1;
+
+		while (l <= r) {
+			int m = l + (r - l) / 2;
+			if (tutors[m].TutorID == search)
+				return m;
+
+			if (tutors[m].TutorID < search)
+				l = m + 1;
+
+			else
+				r = m - 1;
+		}
+		return -1;
+	}
+
+	void modifyTutor() {
+		int key, loc = -1;
+		bool flag;
+		InsertionSort(1);
+
+		// search for the tutor id's location
+		do {
+			int l = 0, r = length - 1;
+			loc = -1;
+			cout << endl << "What tutor ID you want for modify? ";
+			cin >> key;
+
+			loc = BinarySearchForIndex(key);
+
+			if (loc == -1) {
+				cout << "The ID not found! Try again." << endl;
+			}
+		} while (loc == -1);
+		
+		string date, input, data;
+		float fdata;
+		int idata;
+		time_t t;
+			
+		input = "";
+		data = "";
+		cout << endl << "**** Directly ENTER for unchange data ****" << endl;
+
+		// enter name
+		cin.ignore();
+		cout << "Enter tutor name: ";
+		getline(cin, input);
+		data = tutors[loc].Name;
+
+		if (!input.empty()) {
+			data = input;
+		}
+		tutors[loc].Name = data;
+		
+		// enter join date
+		do {
+			cout << "Enter join date (YYYY-MM-DD): ";
+			getline(cin,date);
+
+			if (!date.empty()) {
+				flag = checkDateValid(date);
+				if(!flag) t = toTimeStamp(date);
+			}
+			else {
+				t = tutors[loc].Join_Date;
+				flag = false;
+			}
+		} while (flag);
+		tutors[loc].Join_Date = t;
+
+		// enter term date
+		do {
+			cout << "Enter termination date (YYYY-MM-DD): ";
+			getline(cin, date);
+
+			if (!date.empty()) {
+				flag = checkDateValid(date);
+				if (!flag) t = toTimeStamp(date);
+			}
+			else {
+				t = tutors[loc].Term_Date;
+				flag = false;
+			}
+		} while (flag);
+		tutors[loc].Term_Date = t;
+
+		// enter hourly rate
+		cout << "Enter hourly rate: ";
+		cin >> input;
+		fdata = tutors[loc].Hourly_Rate;
+
+		if (!input.empty()) {
+			fdata = stof(input);
+		}
+		tutors[loc].Hourly_Rate = fdata;
+
+		// enter phone number
+		cout << "Enter phone number: ";
+		cin >> input;
+		idata = tutors[loc].Phone;
+
+		if (!input.empty()) {
+			idata = stof(input);
+		}
+		tutors[loc].Phone = idata;
+
+		// enter address
+		cin.ignore();
+		cout << "Enter address: ";
+		getline(cin, input);
+		data = tutors[loc].Address;
+
+		if (!input.empty()) {
+			data = input;
+		}
+		tutors[loc].Address = data;
+
+		// enter centre
+		cout << "Enter centre code: ";
+		cin >> input;
+		idata = tutors[loc].Centre_Code;
+
+		if (!input.empty()) {
+			idata = stof(input);
+		}
+		tutors[loc].Centre_Code = idata;
+
+		// enter subject
+		cout << "Enter subject code: ";
+		cin >> input;
+		idata = tutors[loc].Subject_Code;
+
+		if (!input.empty()) {
+			idata = stof(input);
+		}
+		tutors[loc].Subject_Code = idata;
+
+		return;
+	}
+
+	void deleteTutor() {
+		int key, loc = -1;
+		bool flag;
+		InsertionSort(1);
+		Tutors* newData = NULL;
+
+		// search for the tutor id's location
+		do {
+			int l = 0, r = length - 1;
+			loc = -1;
+			cout << endl << "Enter tutor ID you want for deletion: ";
+			cin >> key;
+
+			loc = BinarySearchForIndex(key);
+
+			if (loc == -1) {
+				cout << "The ID not found! Try again." << endl;
+			}
+		} while (loc == -1);
+
+		length--;
+		newData = new Tutors[length];
+		int m = 0;
+
+		for (int i = 0; i < length + 1; i++) {
+			if (i != loc) {
+				newData[m].TutorID = tutors[i].TutorID;
+				newData[m].Name = tutors[i].Name;
+				newData[m].Join_Date = tutors[i].Join_Date;
+				newData[m].Term_Date = tutors[i].Term_Date;
+				newData[m].Hourly_Rate = tutors[i].Hourly_Rate;
+				newData[m].Phone = tutors[i].Phone;
+				newData[m].Address = tutors[i].Address;
+				newData[m].Centre_Code = tutors[i].Centre_Code;
+				newData[m].Subject_Code = tutors[i].Subject_Code;
+				newData[m].Rating = tutors[i].Rating;
+				newData[m].Rating_No = tutors[i].Rating_No;
+				m++;
+			}
+		}
+		tutors = newData;
+
+		cout << endl << "Deletion successful" << endl;
+
+		return;
+	}
+
 }tutorsdata;
 
 // functions
-bool isDateNumber(const string& str) {
-	for (int i = 0; i < str.length(); i++) {
-		if (i != 4 && i != 7) {
-			if (isdigit(str[i]) == 0) return false;
-		}
-	}
-	return true;
-}
-
 void MainMenu_HR() {
 	system("CLS");
 	int choice;
@@ -1179,22 +1477,27 @@ void MainMenu_HR() {
 			cout << "----------------------------------------" <<endl;
 			Tutors data;
 			string date;
-			cout << "Enter tutor ID: ";
-			cin >> data.TutorID;
+			int id;
+
+			do {
+				cout << "Enter tutor ID: ";
+				cin >> id;
+
+				flag = tutorsdata.checkIDValid(id);
+			} while (flag);
+			data.TutorID = id;
+
+			cin.ignore();
 			cout << "Enter tutor name: ";
-			cin >> data.Name;
+			getline(cin, data.Name);
+			
+			// check the validity of date
 			do {
 				cout << "Enter join date (YYYY-MM-DD): ";
+				
 				cin >> date;
-				if (date.length() != 10 || !isDateNumber(date) || stoi(date.substr(0, 4)) - 1900 < 0 || 
-					!(stoi(date.substr(5, 7)) - 1 >= 0 && stoi(date.substr(5, 7)) - 1 < 12) ||
-					!(stoi(date.substr(8, 10)) > 0 && stoi(date.substr(8, 10)) <= 31)) {
-					cout << "Invalid format try again!" << endl;
-					flag = true;
-				}
-				else {
-					flag = false;
-				}
+				flag = checkDateValid(date);
+
 			} while (flag);
 			
 			data.Join_Date = toTimeStamp(date);
@@ -1202,8 +1505,9 @@ void MainMenu_HR() {
 			cin >> data.Hourly_Rate;
 			cout << "Enter phone number: ";
 			cin >> data.Phone;
+			cin.ignore();
 			cout << "Enter address: ";
-			cin >> data.Address;
+			getline(cin, data.Address);
 			cout << "Enter centre code: ";
 			cin >> data.Centre_Code;
 			cout << "Enter subject code: ";
@@ -1373,28 +1677,177 @@ void MainMenu_HR() {
 	}
 		break;
 	case 8: {
-		cout << "Modify Tutor Record\n";
-		cout << "----------------------------------------" << endl;
+		do {
+			system("CLS");
+			cout << "Modify Tutor Record\n";
+			cout << "----------------------------------------" << endl;
+		
+			tutorsdata.modifyTutor();
+
+			do {
+				cout << endl << "Enter 1 to repeat again 0 to EXIT: ";
+				cin >> choice;
+			} while (choice < 0 || choice > 1);
+
+			if (choice == 0)
+				MainMenu_HR();
+		} while (choice == 1);
 	}
 		break;
 	case 9: {
-		cout << " 9. Delete Tutor Record\n";
-		cout << "----------------------------------------" << endl;
+		do {
+			system("CLS");
+			cout << "Delete Tutor Record\n";
+			cout << "----------------------------------------" << endl;
+
+			tutorsdata.deleteTutor();
+
+			do {
+				cout << endl << "Enter 1 to repeat again 0 to EXIT: ";
+				cin >> choice;
+			} while (choice < 0 || choice > 1);
+
+			if (choice == 0)
+				MainMenu_HR();
+		} while (choice == 1);
 	}
 		break;
 	case 10: {
-		cout << " 10. Add New User\n";
-		cout << "----------------------------------------" << endl;
+		do {
+			system("CLS");
+			cout << "Add New User\n";
+			cout << "----------------------------------------" << endl;
+			Users data;
+			string pw, role;
+			int id;
+
+			do {
+				cout << "Enter user ID: ";
+				cin >> id;
+
+				flag = usersdata.checkIDValid(id);
+			} while (flag);
+			data.UserID = id;
+
+			cin.ignore();
+			cout << "Enter user name: ";
+			getline(cin, data.Username);
+
+			do {
+				cout << "Enter password: ";
+				cin >> pw;
+
+				remove(pw.begin(), pw.end(), ' ');
+				if (pw.length() < 8) {
+					cout << " **Password need to have 8 characters." << endl;
+					flag = true;
+				}
+				else {
+					flag = false;
+				}
+			} while (flag);
+			data.Password = pw;
+
+			do {
+				cout << "Enter role (student, admin or HR): ";
+				cin >> role;
+
+				if (role != "STUDENT" && role != "student" && role != "Student" &&
+					role != "ADMIN" && role != "admin" && role != "Admin" &&
+					role != "HR MANAGER" && role != "hr manager" && role != "hr" && role != "HR" && role != "Hr Manager") {
+					flag = true;
+					cout << " **The role is invalid!" << endl;
+				}
+				else
+					flag = false;
+
+			} while (flag);
+			data.Role = role;
+			
+			cout << "Enter centre code: ";
+			cin >> data.Centre_Code;
+
+			usersdata.addUserData(data);
+
+			do {
+				cout << endl << "Enter 1 to repeat again 0 to EXIT: ";
+				cin >> choice;
+			} while (choice < 0 || choice > 1);
+
+			if (choice == 0)
+				MainMenu_HR();
+		} while (choice == 1);
 	}
 		break;
 	case 11: {
-		cout << " 11. Add New Centre\n";
-		cout << "----------------------------------------" << endl;
+		do {
+			system("CLS");
+			cout << "Add New Centre\n";
+			cout << "----------------------------------------" << endl;
+			Centre data;
+			int id;
+
+			do {
+				cout << "Enter centre code: ";
+				cin >> id;
+
+				flag = centredata.checkIDValid(id);
+			} while (flag);
+			data.CentreCode = id;
+
+			cin.ignore();
+			cout << "Enter centre name: ";
+			getline(cin, data.Centre_Name);
+
+			cout << "Enter address: ";
+			getline(cin, data.Address);
+
+			cout << "Enter region: ";
+			getline(cin, data.Region);
+
+			centredata.addCentreData(data);
+
+			do {
+				cout << endl << "Enter 1 to repeat again 0 to EXIT: ";
+				cin >> choice;
+			} while (choice < 0 || choice > 1);
+
+			if (choice == 0)
+				MainMenu_HR();
+		} while (choice == 1);
 	}
 		break; 
 	case 12: {
-		cout << " 12. Add New Subject\n";
-		cout << "----------------------------------------" << endl;
+		do {
+			system("CLS");
+			cout << "Add New Subject\n";
+			cout << "----------------------------------------" << endl;
+
+			Subject data;
+			int id;
+
+			do {
+				cout << "Enter subject code: ";
+				cin >> id;
+
+				flag = subjectdata.checkIDValid(id);
+			} while (flag);
+			data.SubjectCode = id;
+
+			cin.ignore();
+			cout << "Enter subject name: ";
+			getline(cin, data.Subject_Name);
+
+			subjectdata.addSubjectData(data);
+
+			do {
+				cout << endl << "Enter 1 to repeat again 0 to EXIT: ";
+				cin >> choice;
+			} while (choice < 0 || choice > 1);
+
+			if (choice == 0)
+				MainMenu_HR();
+		} while (choice == 1);
 	}
 		break;
 	case 0:
